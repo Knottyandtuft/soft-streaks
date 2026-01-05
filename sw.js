8f208e534c9491ea07531d034490105e7e7cf888
Knottyandtuft <knottyandtuft@gmail.com>
2026-01-05 02:57:02 -0800

    Bump cache to v2

--- sw.js
+++ sw.js
@@ -1,18 +1,26 @@
-const CACHE_NAME = "soft-streaks-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+const CACHE_NAME = "soft-streaks-v2"; // <-- bump version to force refresh
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
